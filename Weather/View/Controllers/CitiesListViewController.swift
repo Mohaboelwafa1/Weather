79f85d9d229cc4c4323372cf5bd47cc4336cf8d8
Mohammed Hassan <mohaboelwafa1@gmail.com>
2020-08-08 02:24:59 +0200

    Add refresh control

--- Weather/View/Controllers/CitiesListViewController.swift
+++ Weather/View/Controllers/CitiesListViewController.swift
@@ -16,11 +16,22 @@ class CitiesListViewController: UIViewController {
     @IBOutlet weak var listOfCitiesTable : UITableView!
     var citiesListViewModel: CitiesListViewModel_View  = CitiesListViewModel_Model()
     var distinictCitiesList : Results<CitiesDBModel>?
+    private let refreshControl = UIRefreshControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         listOfCitiesTable.register(CustomCityCell.self, forCellReuseIdentifier: "CustomCityCell")
         listOfCitiesTable.register(UINib(nibName: "CustomCityCell",bundle: nil), forCellReuseIdentifier: "CustomCityCell")
+        // Add Refresh Control to Table View
+        if #available(iOS 10.0, *) {
+            listOfCitiesTable.refreshControl = refreshControl
+        } else {
+            listOfCitiesTable.addSubview(refreshControl)
+        }
+        // Configure Refresh Control
+        refreshControl.addTarget(self, action: #selector(refreshWeatherData(_:)), for: .valueChanged)
+        refreshControl.tintColor = UIColor(red:0.25, green:0.72, blue:0.85, alpha:1.0)
+
         DispatchQueue.global(qos: .background).async {
             self.citiesListViewModel.getCitiesList(completionHandler: {
                 (result, statusCode, errorModel)in
@@ -29,6 +40,18 @@ class CitiesListViewController: UIViewController {
             })
         }
 
+    }
+
+    @objc private func refreshWeatherData(_ sender: Any) {
+        // Fetch Weather Data
+        DispatchQueue.global(qos: .background).async {
+            self.citiesListViewModel.getCitiesList(completionHandler: {
+                (result, statusCode, errorModel)in
+                self.distinictCitiesList = result
+                self.listOfCitiesTable.reloadData()
+                self.refreshControl.endRefreshing()
+            })
+        }
     }
 
     func ConvertTempreture(temp: Double , type: String) -> Double {
