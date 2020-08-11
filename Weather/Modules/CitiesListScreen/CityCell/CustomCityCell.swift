0b69e6a7f463a154cd33477259a677d32f90b30d
Mohammed Hassan <mohaboelwafa1@gmail.com>
2020-08-11 22:24:48 +0200

    Make background image rounded and has a shadow

--- Weather/Modules/CitiesListScreen/CityCell/CustomCityCell.swift
+++ Weather/Modules/CitiesListScreen/CityCell/CustomCityCell.swift
@@ -27,14 +27,30 @@ class CustomCityCell : UITableViewCell {
         self.timeLabel.text = String(describing: model.currentTime)
         self.cityNameLabel.text = String(describing: model.cityName)
         self.temperatureLabel.text = String(describing: model.temp)
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
-        self.downloadImageForCity(url: model.backGroundImage, imageview: imageView)
-        imageView.alpha = 0.9
-        self.backgroundView = UIView()
-        self.backgroundView!.addSubview(imageView)
+        self.setBackgroundImage(url: model.backGroundImage)
     }
 
-    func downloadImageForCity(url : String, imageview : UIImageView) {
+    func setBackgroundImage(url : String) {
+        let outerView = UIView(frame: CGRect(x: 10, y: 10, width: self.frame.width - 20, height: self.frame.height - 20))
+        outerView.clipsToBounds = false
+        outerView.layer.shadowColor = UIColor.black.cgColor
+        outerView.layer.shadowOpacity = 1
+        outerView.layer.shadowOffset = CGSize.zero
+        outerView.layer.shadowRadius = 10
+        outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: 10).cgPath
+
+        let imageView = UIImageView(frame: outerView.bounds)
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        self.downloadImage(url: url, imageview: imageView)
+        imageView.alpha = 0.9
+
+        outerView.addSubview(imageView)
+        self.backgroundView = UIView()
+        self.backgroundView!.addSubview(outerView)
+    }
+
+    func downloadImage(url : String, imageview : UIImageView) {
         let url = URL(string: url)
         let processor = DownsamplingImageProcessor(size: imageview.bounds.size)
         imageview.kf.indicatorType = .activity
