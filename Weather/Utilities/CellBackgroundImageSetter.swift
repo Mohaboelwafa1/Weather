871a82497c718045ccd14953cc60a73b4ef997fb
Mohammed Hassan <mohaboelwafa1@gmail.com>
2020-09-18 21:08:05 +0200

    Make setbackground for cell working with both tables and collections

--- Weather/Utilities/CellBackgroundImageSetter.swift
+++ Weather/Utilities/CellBackgroundImageSetter.swift
@@ -11,24 +11,33 @@ import UIKit
 
 class CellBackgroundImageSetter {
 
-    static func setBackgroundImage(imageURL : String, cell: UITableViewCell) {
-        let outerView = UIView(frame: CGRect(x: 10, y: 10, width: cell.frame.width - 20, height: cell.frame.height - 20))
-        outerView.clipsToBounds = false
-        outerView.layer.shadowColor = UIColor.black.cgColor
-        outerView.layer.shadowOpacity = 1
-        outerView.layer.shadowOffset = CGSize.zero
-        outerView.layer.shadowRadius = 10
-        outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: 10).cgPath
+    static func setBackgroundImage(imageURL : String, cell: UIView) {
+        if cell.isKind(of: UITableViewCell.self) || cell.isKind(of: UICollectionViewCell.self) {
+            let outerView = UIView(frame: CGRect(x: 10, y: 10, width: cell.frame.width - 20, height: cell.frame.height - 20))
+            outerView.clipsToBounds = false
+            outerView.layer.shadowColor = UIColor.black.cgColor
+            outerView.layer.shadowOpacity = 1
+            outerView.layer.shadowOffset = CGSize.zero
+            outerView.layer.shadowRadius = 10
+            outerView.layer.shadowPath = UIBezierPath(roundedRect: outerView.bounds, cornerRadius: 10).cgPath
 
-        let imageView = UIImageView(frame: outerView.bounds)
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 10
-        ImageDownloader.downloadImage(url: imageURL, imageview: imageView)
-        imageView.alpha = 0.9
+            let imageView = UIImageView(frame: outerView.bounds)
+            imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = 10
+            ImageDownloader.downloadImage(url: imageURL, imageview: imageView)
+            imageView.alpha = 0.9
 
-        outerView.addSubview(imageView)
-        cell.backgroundView = UIView()
-        cell.backgroundView!.addSubview(outerView)
+            outerView.addSubview(imageView)
+            if cell.isKind(of: UITableViewCell.self) {
+                (cell as! UITableViewCell).backgroundView = UIView()
+                (cell as! UITableViewCell).backgroundView!.addSubview(outerView)
+            } else {
+                (cell as! UICollectionViewCell).backgroundView = UIView()
+                (cell as! UICollectionViewCell).backgroundView!.addSubview(outerView)
+            }
+
+        }
+
     }
 
 }
